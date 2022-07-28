3acc1b60df03c9ff6c642481ebdcdc86ccdc91ac
«VendyVL» <vedmansk@yandex.ru>
2022-07-28 22:43:44 +0300

    первый

--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -1,14 +1,13 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-text = ('абв где опрос по слово п680сн "Забвение" про забава').split(" ")
+text = 'абв где опрос по слово п680сн "Забвение" про забава лабва'.split(" ")
 
-a = []
-for i in range(len(text)):
-    if text[i].find('абв') >= 0: 
-        a.append(i)
+a = [i for i in range(len(text)) if text[i].find('абв') >= 0]
 
-print(a)
-for i in a:
-    text.pop(i)
+a.reverse() # Чтобы удаляло с конца и не вышло за
+
+for i in range (len(a)):
+    del text[a[i]]
 
 print (text)
+
